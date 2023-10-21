b040479c7f1355055fa6b75eccebaaee9c618f1c
mccantsquincy <quincymccants@gmail.com>
2023-10-20 21:32:03 -0700

    tweaking logo for dark and light theme

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -10,12 +10,13 @@ const Navbar = () => {
     const scrolled = useScrollToTop();
 
     return ( 
-        <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6 gap-4", scrolled && "border-b shadow-sm")}>
+        <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
               Login
+              <ModeToggle />
             </div>
-            <ModeToggle />
+            
         </div>
      );
 }
