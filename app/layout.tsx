51910cef0e7a9892f0c348467dec79949b01f015
mccantsquincy <quincymccants@gmail.com>
2023-10-21 21:10:08 -0700

    config convex and clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,32 +21,32 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-light.svg",
         href: "/logo-light.svg",
-      }
-    ]
+      },
+    ],
   },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-      <ThemeProvider
-         attribute="class"
-         defaultTheme="system"
-         enableSystem
-         disableTransitionOnChange
-         storageKey="jotion-theme-2"
-      >
+        <ConvexClientProvider>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
             disableTransitionOnChange
-        {children}
-      </ThemeProvider>
+            storageKey="jotion-theme-2"
+          >
+            attribute="class" defaultTheme="system" enableSystem
+            disableTransitionOnChange
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   );
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://cool-gorilla-1.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
