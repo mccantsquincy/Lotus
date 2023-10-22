bd5c9e1325d690dc4e8ca513506b3e3ea857371e
mccantsquincy <quincymccants@gmail.com>
2023-10-21 21:40:58 -0700

    set header btn for auth and authorization

--- app/(marketing)/_components/Header.tsx
+++ app/(marketing)/_components/Header.tsx
@@ -1,21 +1,49 @@
+"use client";
+
+import { useConvexAuth } from "convex/react";
+
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/clerk-react";
 
 const Header = () => {
-    return ( 
-        <div className="max-w-3xl space-y-4">
-           <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-             Your personal workspace Unified. <span className="underline">Lotus</span>
-           </h1>
-           <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-            Productive, Organized, Efficient, and Free is what <br/> Lotus stands for.
-           </h3>
-           <Button>
-             Use Lotus
-             <ArrowRight className="h-4 w-4 ml-2"/>
-           </Button>
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your personal workspace Unified.{" "}
+        <span className="underline">Lotus</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Productive, Organized, Efficient, and Free is what <br /> Lotus stands
+        for.
+      </h3>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
         </div>
-     );
-}
- 
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Use Lotus
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton>
+          <Button>
+            Use Lotus Free
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
+    </div>
+  );
+};
+
 export default Header;
