289d19f94b54d687987b10b1d5bc3cef6e90b750
mccantsquincy <quincymccants@gmail.com>
2023-10-23 11:14:20 -0700

    implemented sidebar dropdown menu, and logout

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,28 @@
 "use client";
+// note change work image to empty image
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
 
 const DocumentsPage = () => {
+    const { user } = useUser();
     return (
-        <div>
-            This is a protected DocumentsPage
+        <div className="h-full flex flex-col items-center justify-center space-y-4">
+            <Image 
+               src="/work.png"
+               height="300"
+               width="300"
+               alt="working"
+            />
+            <h2 className="text-lg font-md">
+                Welcome to {user?.firstName}&apos;s Lotus workspace
+            </h2>
+            <Button>
+              <PlusCircle className="h-4 w-4 mr-2" />
+              Create a note  
+            </Button>
         </div>
     )
 }
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
+import UserItem from "./user-item";
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -97,8 +98,6 @@ export const Navigation = () => {
     }
   }; 
 
-
-
   return (
     <>
       <aside
@@ -120,7 +119,7 @@ export const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
