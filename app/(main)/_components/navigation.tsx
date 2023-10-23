faeba0a8850d200c66c3f2eee0c9ffe5c8811116
mccantsquincy <quincymccants@gmail.com>
2023-10-23 09:58:30 -0700

    implemented resizing of sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon } from "lucide-react";
+import { ChevronsLeft, MenuIcon, Sidebar } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
@@ -17,6 +17,21 @@ export const Navigation = () => {
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
+  useEffect(() => {
+    if(isMobile) {
+        collapse();
+    } else {
+        resetWidth();
+    }
+  }, [isMobile])
+
+  useEffect(() => {
+    if(isMobile) {
+        collapse();
+    }
+  }, [pathname, isMobile])
+
+  // once mouse down the resizing logic will be available
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
@@ -28,13 +43,61 @@ export const Navigation = () => {
     document.addEventListener("mouseup", handleMouseUp);
   }
 
-  const handleMouseMove = () => {
-    
-  }
+  // allowing the resizing of the sidebar
+  const handleMouseMove = (event: MouseEvent) => {
+    if(!isResizingRef.current) return;
+    let newWidth = event.clientX;
 
+    if(newWidth < 240) newWidth = 240;
+    if(newWidth > 480) newWidth = 480;
+
+    if(sidebarRef.current && navbarRef.current) {
+        sidebarRef.current.style.width = `${newWidth}px`;
+        navbarRef.current.style.setProperty("left", `${newWidth}px`);
+        navbarRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+    }
+  };
+
+  // removing the resizing funcitonality
   const handleMouseUp = () => {
+    isResizingRef.current = false;
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  };
 
-  }
+  const resetWidth = () => {
+    if(sidebarRef.current && navbarRef.current) {
+        setIsCollapsed(false);
+        setIsResetting(true);
+
+        sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+        navbarRef.current.style.setProperty(
+            "width",
+            isMobile ? "0" : "calc(100% - 240px)"
+        );
+        navbarRef.current.style.setProperty(
+            "left",
+            isMobile ? "100%" : "240px"
+        );
+
+        setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
+  const collapse = () => {
+    if(sidebarRef.current && navbarRef.current) {
+        setIsCollapsed(true);
+        setIsResetting(true);
+
+        sidebarRef.current.style.width = "0";
+        navbarRef.current.style.setProperty("width", "100%");
+        navbarRef.current.style.setProperty("left", "0");
+        
+        setTimeout(() => setIsResetting(false), 300);
+    }
+  }; 
+
+
 
   return (
     <>
@@ -48,6 +111,7 @@ export const Navigation = () => {
       >
         <div
           role="button"
+          onClick={collapse}
           className={cn(
             "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
             isMobile && "opacity-100"
@@ -63,7 +127,7 @@ export const Navigation = () => {
         </div>
         <div 
           onMouseDown={handleMouseDown}
-          onClick={() => {}}
+          onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" 
         />
       </aside>
@@ -77,7 +141,7 @@ export const Navigation = () => {
       >
         <nav className="bg-transparent px-3 py-2 w-full">
           {isCollapsed && (
-            <MenuIcon role="button" className="h-6 w-6 text-muted-foreground" />
+            <MenuIcon onClick={resetWidth} role="button" className="h-6 w-6 text-muted-foreground" />
           )}
         </nav>
       </div>
