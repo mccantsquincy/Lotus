fdc9f10beda4fa0591b912b205ba154622503746
mccantsquincy <quincymccants@gmail.com>
2023-10-24 22:19:36 -0700

    documenting schema and APIs

--- convex/documents.ts
+++ convex/documents.ts
@@ -3,7 +3,10 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
 
-// this function get documents form convex
+/* 
+  This is a `get` request function that first checks to see if its an authenticated user
+  once user is verified it will query the database for the documents and return the documents.
+*/ 
 export const get = query({
     handler: async (ctx) => {
         const identity = await ctx.auth.getUserIdentity();
@@ -18,7 +21,13 @@ export const get = query({
     }
 })
 
-// this function creates documents in convex
+/*
+  This function creates documents in convex
+  In summary, this code defines a function `create` that expects a title and an optional parent document ID. 
+  It chcks if a user is authenticated, gets the users ID, and then inserts a new document into a database. 
+  The newly created document is then returned
+*/ 
+
 export const create = mutation({
     args: {
         title: v.string(),
@@ -41,7 +50,7 @@ export const create = mutation({
             isPublished: false,
         });
 
-        return document
+        return document;
     }
 })
 
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,6 +1,15 @@
 import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
+/*
+  defineSchema is creating a schema object. it takes an argument which is
+  an object containing one table "documents". 
+  defineTable is used to define the properties of a table.
+  We are also adding two indexes to the table for userId column and 
+  efficient quering based on userId and a combination of parent document. 
+  Indexes help in optimizing queries
+*/
+
 export default defineSchema({
     documents: defineTable({
         title: v.string(),
