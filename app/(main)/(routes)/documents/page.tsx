45d733de3c80e0f010ebcbde7da851181a31ba09
mccantsquincy <quincymccants@gmail.com>
2023-10-24 17:14:17 -0700

    implemented schema and convex api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -5,9 +5,25 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircle } from "lucide-react";
+import { useMutation } from "convex/react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
 
 const DocumentsPage = () => {
     const { user } = useUser();
+    const create = useMutation(api.documents.create);
+
+    const onCreate = () => {
+      const promise = create({ title: "Untitled" });
+
+      toast.promise(promise, {
+        loading: "Creating a new note...",
+        success: "New note ctreated!",
+        error: "Failed to create a new note."
+      });
+    };
+
     return (
         <div className="h-full flex flex-col items-center justify-center space-y-4">
             <Image 
@@ -19,7 +35,7 @@ const DocumentsPage = () => {
             <h2 className="text-lg font-md">
                 Welcome to {user?.firstName}&apos;s Lotus workspace
             </h2>
-            <Button>
+            <Button onClick={onCreate}>
               <PlusCircle className="h-4 w-4 mr-2" />
               Create a note  
             </Button>
