a72aa7528dd9fbb6c0f212a32ecd3a42eed6d856
mccantsquincy <quincymccants@gmail.com>
2023-10-27 21:35:52 -0700

    working on sidebar item component

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Id } from "@/convex/_generated/dataModel";
-import { LucideIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -15,18 +16,48 @@ interface ItemProps {
   onClick: () => void;
   icon: LucideIcon;
 }
-const Item = ({ label, onClick, icon: Icon }: ItemProps) => {
+const Item = ({ 
+  id,
+  label, 
+  onClick, 
+  icon: Icon,
+  active,
+  documentIcon,
+  isSearch,
+  level = 0,
+  onExpand,
+  expanded,
+}: ItemProps) => {
+  const ChevronIcon = expanded ? ChevronDown : ChevronRight; 
+
+
+
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ paddingLeft: "12px" }}
-      className="group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium"
+      style={{ 
+        paddingLeft: level ? `${(level * 12) + 12}px` : "12px"
+      }}
+      className={cn(
+        "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
+        active && "bg-primary/5 text-primary"
+      )}
     >
+      {!!id && (
+        <div
+          role="button"
+          className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+        >
+          <ChevronIcon />
+        </div>
+      )
+
+      }
       <Icon className="shrink-0 h-[18px] text-muted-foreground" />
       <span className="truncate">{label}</span>
     </div>
-  );
+  ); 
 };
 
 export default Item;
