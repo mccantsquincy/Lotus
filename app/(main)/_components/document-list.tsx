8c8f5734255e5c076b268c55db88771c484b63ef
mccantsquincy <quincymccants@gmail.com>
2023-11-01 21:37:39 -0700

    working on side bar doc item

--- app/(main)/_components/document-list.tsx
+++ app/(main)/_components/document-list.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import Item from "./item";
+import { cn } from "@/lib/utils";
+import { FileIcon } from "lucide-react";
 
 interface DocumentListProps {
   parentDocumentId?: Id<"documents">;
@@ -13,6 +15,9 @@ interface DocumentListProps {
   data?: Doc<"documents">[];
 }
 
+/*
+
+*/
 export const DocumentList = ({
   parentDocumentId,
   level = 0,
@@ -50,7 +55,40 @@ export const DocumentList = ({
     );
   }
 
-  return <div>Document list</div>;
+  return (
+    <>
+      <p
+        style={{
+          paddingLeft: level ? `${level * 12 + 25}px` : undefined,
+        }}
+        className={cn(
+          "hidden text-sm font-medium text-muted-foreground/80",
+          expanded && "last:block",
+          level === 0 && "hidden"
+        )}
+      >
+        No pages inside
+      </p>
+      {documents.map((document) => (
+        <div key={document._id}>
+          <Item
+            id={document._id}
+            onClick={() => onRedirect(document._id)}
+            label={document.title}
+            icon={FileIcon}
+            documentIcon={document.icon}
+            active={params.documentId == document._id}
+            level={level}
+            onExpand={() => onExpand(document._id)}
+            expanded={expanded[document._id]}
+          />
+          {expanded[document._id] && (
+            <DocumentList parentDocumentId={document._id} level={level + 1} />
+          )}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default DocumentList;
